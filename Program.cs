5722ad1c181ec5199f2fb0d534d87769ed89a716
Victor <victorrefundine@hotmail.com>
2023-11-26 15:59:36 -0300

    Adição de programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("      Entrada Válida\n");
+
+int numeroDigitado = -1;
+
+while (numeroDigitado < 0 || numeroDigitado > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    numeroDigitado = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numeroDigitado == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {numeroDigitado}");
+}
+
+Console.WriteLine("\nVolte sempre!");
